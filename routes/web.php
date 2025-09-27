43cf1e2cc88ff8b2a148c0c8e0c3d985c10f4867
Yosa-web <mys310503@gmail.com>
2025-09-28 00:22:25 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('landing.welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/tentang', function () {
+    return view('landing.tentang');
+});
+
+Route::get('/layanan', function () {
+    return view('landing.layanan');
+});
+
+Route::get('/blog', function () {
+    return view('landing.blog');
+});
+
+Route::get('/sejarah', function () {
+    return view('landing.sejarah');
+});
+
+Route::get('/visimisi', function () {
+    return view('landing.visimisi');
+});
+
+Route::get('/bidangusaha', function () {
+    return view('landing.bidangusaha');
+});
+
+Route::get('/foto', function () {
+    return view('landing.foto');
+});
+
+Route::get('/video', function () {
+    return view('landing.video');
+});
+
+Route::get('/kontak', function () {
+    return view('landing.kontak');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
